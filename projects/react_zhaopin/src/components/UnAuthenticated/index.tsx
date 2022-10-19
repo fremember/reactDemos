4515d482c0ffa254f27f5585492464556501408d
Vincent <605941383@qq.com>
2022-10-19 23:13:14 +0800

    修改热区大小

--- projects/react_zhaopin/src/components/UnAuthenticated/index.tsx
+++ projects/react_zhaopin/src/components/UnAuthenticated/index.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react'
 
+import commonStyles from 'assets/css/common.module.css'
 import styles from './index.module.scss'
 
 import { Login, Register } from 'pages'
@@ -10,7 +11,14 @@ export const UnAuthenticated: React.FC = () => {
 		<div className={ styles.unAuthenticated }>
 			<div>{ isRegister ? '请注册' : '请登录' }</div>
 			{ isRegister ? <Login /> : <Register /> }
-			<p onClick={ () => setIsRegister(!isRegister) }>{ isRegister ? '已经有账号了？直接登录' : '没有账号？注册新账号' }</p>
+			<div className={ commonStyles.dib } onClick={ () => setIsRegister(!isRegister) }>
+				{/* { isRegister ? '已经有账号了？<span>直接登录</span>' : '没有账号？注册新账号' } */}
+				{ isRegister ? 
+					<p>已经有账号了？<span className={ commonStyles.color08c }>直接登录</span></p>
+					:
+					<p>没有账号？<span className={ commonStyles.color08c }>注册新账号</span></p>
+				}
+			</div>
 		</div>
 	)
 }

d1266fe2e5954a71cf0d6dd60e36600141048061
Vincent <605941383@qq.com>
2022-10-20 10:13:15 +0800

    修改登录和注册公共部分

--- projects/react_zhaopin/src/components/UnAuthenticated/index.tsx
+++ projects/react_zhaopin/src/components/UnAuthenticated/index.tsx
@@ -8,17 +8,18 @@ import { Login, Register } from 'pages'
 export const UnAuthenticated: React.FC = () => {
 	const [ isRegister, setIsRegister ] = useState<boolean>(false)
 	return (
-		<div className={ styles.unAuthenticated }>
-			<div>{ isRegister ? '请注册' : '请登录' }</div>
-			{ isRegister ? <Login /> : <Register /> }
-			<div className={ commonStyles.dib } onClick={ () => setIsRegister(!isRegister) }>
-				{/* { isRegister ? '已经有账号了？<span>直接登录</span>' : '没有账号？注册新账号' } */}
-				{ isRegister ? 
-					<p>已经有账号了？<span className={ commonStyles.color08c }>直接登录</span></p>
-					:
-					<p>没有账号？<span className={ commonStyles.color08c }>注册新账号</span></p>
-				}
-			</div>
+		<div className={ `${commonStyles['flex-center']} ${styles.unAuthenticated}` }>
+			<section className={ commonStyles.flex} >
+				<div className={ `${styles.title} ${commonStyles['common-active']}` }>{ isRegister ? '请注册' : '请登录' }</div>
+				{ isRegister ? <Login /> : <Register /> }
+				<div className={ commonStyles.dib } onClick={ () => setIsRegister(!isRegister) }>
+					{ isRegister ? 
+						<p>已经有账号了？<span className={ commonStyles['common-active'] }>直接登录</span></p>
+						:
+						<p>没有账号？<span className={ commonStyles['common-active'] }>注册新账号</span></p>
+					}
+				</div>
+			</section>
 		</div>
 	)
 }

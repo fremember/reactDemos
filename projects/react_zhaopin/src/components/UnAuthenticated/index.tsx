f768b904b8db90426e365489d38d107b4d5be999
Vincent <605941383@qq.com>
2022-10-22 09:45:46 +0800

    修改未授权公共样式

--- projects/react_zhaopin/src/components/UnAuthenticated/index.tsx
+++ projects/react_zhaopin/src/components/UnAuthenticated/index.tsx
@@ -6,20 +6,36 @@ import styles from './index.module.scss'
 import { Login, Register } from 'pages'
 
 export const UnAuthenticated: React.FC = () => {
-	const [ isRegister, setIsRegister ] = useState<boolean>(false)
+	const [ isRegister, setIsRegister ] = useState<boolean>(false),
+		changeUnAuthenticatedTab: React.MouseEventHandler<HTMLUListElement> | undefined = (e: React.MouseEvent<HTMLUListElement, MouseEvent>) => {
+			// if((e.target as HTMLElement).nodeName === 'LI') {
+			// 	console.log(e)
+			// }
+			console.log((e.target as HTMLElement).className)
+		};
 	return (
 		<div className={ `${commonStyles['flex-center']} ${styles.unAuthenticated}` }>
 			<section className={ commonStyles.flex} >
-				<div className={ `${styles.title} ${commonStyles['common-active']}` }>{ isRegister ? '请注册' : '请登录' }</div>
+				<ul className={ `${commonStyles.flex} ${commonStyles.colorfff} ${styles.tabs}` } onClick={ changeUnAuthenticatedTab }>
+					<li className={ !isRegister ? styles.active : '' }>登录</li>
+					<li className={ isRegister ? styles.active : '' }>注册</li>
+				</ul>
 				{ isRegister ? <Login /> : <Register /> }
-				<div className={ commonStyles.dib } onClick={ () => setIsRegister(!isRegister) }>
+				{/* <div className={ `${commonStyles.dib} ${commonStyles.colorfff} ${styles['about-count']}` } onClick={ () => setIsRegister(!isRegister) }>
 					{ isRegister ? 
 						<p>已经有账号了？<span className={ commonStyles['common-active'] }>直接登录</span></p>
 						:
 						<p>没有账号？<span className={ commonStyles['common-active'] }>注册新账号</span></p>
 					}
-				</div>
+				</div> */}
 			</section>
 		</div>
 	)
 }
+
+
+
+
+
+
+
